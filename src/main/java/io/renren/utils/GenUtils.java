c426208676dc4488a3f315bcbdca197c0657a36a
ZhaoWenHao1 <happyzhao1010@gmail.com>
2022-03-02 23:00:00 +0800

    完善模板

--- src/main/java/io/renren/utils/GenUtils.java
+++ src/main/java/io/renren/utils/GenUtils.java
@@ -326,7 +326,7 @@ public class GenUtils {
             return packagePath + "entity" + File.separator + "inner" + File.separator + currentTableName+ File.separator + splitInnerName(className)+ "InnerEntity.java";
         }
         if (template.contains("EntityJpa.java.vm") || template.contains("EntityMapper.java.vm") || template.contains("MongoEntity.java.vm")) {
-            return packagePath + "entity" + File.separator + className + "Entity.java";
+            return packagePath + "entity" + File.separator + className + ".java";
         }
 
         if (template.contains("Dao.java.vm")) {
